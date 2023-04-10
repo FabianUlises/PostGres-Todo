4ebe1521db92ec86c78f7072e045cf7148f3fffa
FabianUlises <ulisesfab.car@outlook.com>
2023-04-10 07:22:36 -0700

    Added buttons to list header component

--- client/src/components/listheader/ListHeader.js
+++ client/src/components/listheader/ListHeader.js
@@ -3,9 +3,17 @@ import React from 'react';
 // Styles
 import styles from './listheader.css';
 const ListHeader = (props) => {
+  // Function to handle signout button
+  const signOut = () => {
+    console.log('signout');
+  };
   return (
     <div className='list-header'>
-      {props.title}
+      <h1>{props.title}</h1>
+      <div className='button-container'>
+        <button className='create-btn' type='button'>Add New</button>
+        <button onClick={signOut} className='signout-btn' type='button'>Sign Out</button>
+      </div>
     </div>
   );
 };

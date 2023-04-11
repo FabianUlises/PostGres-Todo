8609f38087f64d6ec9aee1a8b9f81f25a5dc9a0d
FabianUlises <ulisesfab.car@outlook.com>
2023-04-10 17:53:56 -0700

    Connected db to server. Able to access data

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+// Postgres db connection
+const pool = require('./db');
 // Routes
-app.get('/', (req, res) => {
-    res.send('/ root stub route');
+app.get('/', async(req, res) => {
+    try {
+        const todos = await pool.query('SELECT * FROM todos');
+        res.json(todos.rows);
+    }catch(err) {
+        console.log(err);
+    }
 });
 // Server on
 app.listen(process.env.PORT,() => console.log('server on'));

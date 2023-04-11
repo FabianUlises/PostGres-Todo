db1547b51ff43baca32a31e824519f357af7c46a
FabianUlises <ulisesfab.car@outlook.com>
2023-04-10 23:50:26 -0700

    Added styles to buttons

--- client/src/components/listitem/ListItem.js
+++ client/src/components/listitem/ListItem.js
@@ -1,11 +1,22 @@
 // Dependencies
 import React from 'react';
+// Components
+import TickIcon from './../tickicon/TickIcon';
+import Progressbar from './../progressbar/Progressbar';
 // Styles
 import styles from './listitem.css';
 const ListItem = (props) => {
   return (
     <div className='list-item'>
-      {props.title}
+      <div className='list-item__content'>
+        <TickIcon />
+        <p className='item-title'>{props.title}</p>
+        <Progressbar />
+      </div>
+      <div className='list-item__button-container'>
+        <button className='edit-btn'>Edit</button>
+        <button className='delete-btn'>Delete</button>
+      </div>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 // Routes
 app.get('/todos/:userEmail', async(req, res) => {
     const { userEmail } = req.params;
-    console.log(req);
+    console.log(userEmail);
     try {
         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
         res.json(todos.rows);

c3519428b7f257afb50b627bc21ed915b319805d
FabianUlises <ulisesfab.car@outlook.com>
2023-04-10 21:43:17 -0700

    Created function to display todos from backend or display loading todos if there are no todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 // Dependencies
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 // Components
 import ListHeader from './components/listheader/ListHeader';
 function App() {
+  // State
+  const [todos, setTodos] = useState(null);
+  // User email
+  const userEmail = 'user@test.com';
   // Fetching data from backend
   const getData = async() => {
-    const userEmail = 'user@test.com';
     try {
+      // Fetching data
       const res = await fetch(`http://localhost:4001/todos/${userEmail}`);
       const data = await res.json();
-      console.log(data);
+      // Updating state with data from back end
+      setTodos(data);
     }catch(err) {
       console.log(err);
     }
   }
   // Useeffect
   useEffect(() => {
+    // Fetching data
     getData();
   }, []);
+  // Display todos on dom if there is todos else rendering 'loading todos'
+  const displayTodos = !todos ? <p>loading todos...</p> : todos.map((todo) => (
+    <div>
+      {todo.title}
+    </div>
+  ));
   return (
     <div className="App">
       <ListHeader title='📃 Current Tasks' />
+      {displayTodos}
     </div>
   );
 }

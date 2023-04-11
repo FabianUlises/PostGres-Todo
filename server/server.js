30d37fa1afa19acecf3e9372126bc080e4f41f59
FabianUlises <ulisesfab.car@outlook.com>
2023-04-10 19:14:12 -0700

    Configured cors

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 // Postgres db connection
 const pool = require('./db');
+// Middleware
+app.use(cors());
 // Routes
-app.get('/', async(req, res) => {
+app.get('/todos/:userEmail', async(req, res) => {
+    const { userEmail } = req.params;
+    console.log(req);
     try {
-        const todos = await pool.query('SELECT * FROM todos');
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
         res.json(todos.rows);
     }catch(err) {
         console.log(err);

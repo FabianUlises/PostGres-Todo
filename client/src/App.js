3cce6a420781d05cd96afa0e52b9fe7bd83e1bcb
FabianUlises <ulisesfab.car@outlook.com>
2023-04-10 19:11:03 -0700

    Fetching data from backend to frontend. Need to configure cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 // Dependencies
-import React from 'react';
+import React, { useEffect } from 'react';
 // Components
 import ListHeader from './components/listheader/ListHeader';
 function App() {
+  // Fetching data from backend
+  const getData = async() => {
+    const userEmail = 'user@test.com';
+    try {
+      const res = await fetch(`http://localhost:4001/todos/${userEmail}`);
+      const data = await res.json();
+      console.log(data);
+    }catch(err) {
+      console.log(err);
+    }
+  }
+  // Useeffect
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
       <ListHeader title='📃 Current Tasks' />

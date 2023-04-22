b0b3b941b53fc946493caa18c41e1d804d4ffd38
FabianUlises <ulisesfab.car@outlook.com>
2023-04-22 06:15:58 -0700

    Added comments to code

--- server/server.js
+++ server/server.js
@@ -10,26 +10,35 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json());
 // Routes
+// Get data from db using user email
 app.get('/todos/:userEmail', async(req, res) => {
+    // Getting email parameter from request
     const { userEmail } = req.params;
-    console.log(userEmail);
     try {
+        // Getting data from db using email
         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
+        // Sending back data
         res.json(todos.rows);
     }catch(err) {
         console.error(err);
     }
 });
+// Post task to db
 app.post('/todos', async(req, res) => {
+    // Getting email title and progress from body request to post as task on db
     const { user_email, title, progress } = req.body
+    // Creating unique id for task
     const id = uuidv4()
     try {
+      // Inserting new task to db
       const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title, progress) VALUES($1, $2, $3, $4)`,
         [id, user_email, title, progress])
+      // Sending back data
       res.json(newToDo);
     } catch (err) {
       console.error(err)
     }
   })
 // Server on
-app.listen(process.env.PORT,() => console.log('server on'));
+app.listen(process.env.PORT, () => 
+  console.log(`server listeningn on port: ${process.env.PORT}`));
